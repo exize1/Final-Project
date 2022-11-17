5fdefd3c2eacd15cd371af70156247c40bc42238
Achiya Avrahamof <achiyaav001@gmail.com>
2022-11-17 16:22:51 +0200

    add socket

--- server/index.js
+++ server/index.js
@@ -9,7 +9,30 @@ require('dotenv').config()
 const port = process.env.PORT || 3001
 const app = express()
 app.use(cors())
+/////////socket.io
+const http= require("http")
+const {Server}= require('socket.io')
 
+const server= http.createServer(app)
+const io =new Server(server, {
+  cors:{
+      origin:["http://localhost:3000","http://localhost:3002"],
+      methods:["GET","POST"]
+  },
+})
+
+io.on("connection",(socket)=>{
+  console.log("user connected :"+socket.id);
+
+  socket.on("join_room",(data)=>{
+      socket.join(data)
+  })
+  socket.on("send_msg",(data)=>{
+      socket.to(data.room).emit("recive_msg",data)
+  })
+})
+
+///////////end
 mongoose.Promise = global.Promise
 mongoose.connect( process.env.DB, {useNewUrlParser: true})
     .then(() => console.log('connected to DB'))
@@ -20,7 +43,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use('/api', routes)
 
 
-app.listen( port, () => {
+server.listen( port, () => {
     console.log(`server listening on ${port} `);
 })
 
