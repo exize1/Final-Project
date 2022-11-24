0c638a08503753ed7504ba074811b45348ecdc52
Shahar Zidon <shaharsmacbookpro@Shahars-MacBook-Pro.local>
2022-11-24 11:20:19 +0200

    responsive-volunteering

--- veterinarian/src/Components/AnimalCard/AnimalCard.jsx
+++ veterinarian/src/Components/AnimalCard/AnimalCard.jsx
@@ -18,127 +18,127 @@ function AnimalCard({ report }) {
         setAlert(data.error)
         setAlertType(data.alertType)
         setAlertMessage(data.message)
-        console.log(alert, alertType, alertMessage );
+        console.log(alert, alertType, alertMessage);
     }
     return (
         <>
-        <div className="card mt-4 animal-card">
-            <img src={report.reportDetails.picture.url} className="card-img-top pet-card-img" alt="..." />
+            <div className="card mt-4 animal-card">
+                <img src={report.reportDetails.picture.url} className="card-img-top pet-card-img" alt="..." />
 
-            <div className="card-body">
-                <div className='card-body-details'>
-                    <div className='modal-container-details-text' dir='rtl'>
-                        <p dir='rtl' className='modal-container-details-text-title'>שם מדווח: </p>
-                        <p>{report.reporterDetails.fullName}</p>
-                    </div>
-                    <div className='modal-container-details-text desc' dir='rtl'>
-                        <p dir='rtl' className='modal-container-details-text-title'>תיאור:</p>
-                        <p>{report.reportDetails.details}</p>
-                    </div>
-                    <div className='modal-container-details-text' dir='rtl'>
-                        <p dir='rtl' className='modal-container-details-text-title'>שכונה:</p>
-                        <p>{report.location.neighborhood}</p>
-                    </div>
-                </div>
-                <div className='card-body-datetime'>
-                    <h6 className="card-title-date" dir='rtl'>{report.reportDetails.time.date}</h6>
-                    <h6 className="card-title-time" dir='rtl'>{report.reportDetails.time.hour}</h6>
-                </div>
-                <div className='card-body-btn-container'>
-                    <Modal addOverflow={true} className='animal-modal' dir='rtl' report={report} title='פרטים נוספים:' modalButtonName='פרטים נוספים' time={report.time} size={windowWidth > 500 ? "medium" : "large"} >
-                        <div className='card-body-btn-container-status' dir='rtl'>
-                            <p dir='rtl' className='card-body-btn-container-status-title'>סטאטוס: </p>
-                            <p> {report.status}</p>
+                <div className="card-body">
+                    <div className='card-body-details'>
+                        <div className='modal-container-details-text' dir='rtl'>
+                            <p dir='rtl' className='modal-container-details-text-title'>שם מדווח: </p>
+                            <p>{report.reporterDetails.fullName}</p>
                         </div>
-                        <div className='animal-modal-img'>
-                            <div className='animal-modal-img-container'>
-                                <img className='animal-modal-img-container-image' src={report.reportDetails.picture.url} alt="" />
-                                {/* <Image cloudName="diggwedxe" publicId={user.avatar.public_id} className="img-thumbnail"/> */}
-                            </div>
+                        <div className='modal-container-details-text desc' dir='rtl'>
+                            <p dir='rtl' className='modal-container-details-text-title'>תיאור:</p>
+                            <p>{report.reportDetails.details}</p>
                         </div>
-                        <div className='modal-container-details'>
-                            <div className='modal-container-details-text' dir='rtl'>
-                                <p dir='rtl' className='modal-container-details-text-title'>שם מדווח: </p>
-                                <p>{report.reporterDetails.fullName}</p>
+                        <div className='modal-container-details-text' dir='rtl'>
+                            <p dir='rtl' className='modal-container-details-text-title'>שכונה:</p>
+                            <p>{report.location.neighborhood}</p>
+                        </div>
+                    </div>
+                    <div className='card-body-datetime'>
+                        <h6 className="card-title-date" dir='rtl'>{report.reportDetails.time.date}</h6>
+                        <h6 className="card-title-time" dir='rtl'>{report.reportDetails.time.hour}</h6>
+                    </div>
+                    <div className='card-body-btn-container'>
+                        <Modal addOverflow={true} className='animal-modal' dir='rtl' report={report} title='פרטים נוספים:' modalButtonName='פרטים נוספים' time={report.time} size={windowWidth > 500 ? "medium" : "large"} >
+                            <div className='card-body-btn-container-status' dir='rtl'>
+                                <p dir='rtl' className='card-body-btn-container-status-title'>סטאטוס: </p>
+                                <p> {report.status}</p>
                             </div>
-                            <div className='modal-container-details-text' dir='rtl'>
-                                <p dir='rtl' className='modal-container-details-text-title'>תיאור: </p>
-                                <p>{report.reportDetails.details}</p>
-                            </div>
-
-                            <div className='modal-container-details-short-inputs'>
-                                <div className='modal-container-details-short-inputs-upper row' dir='rtl'>
-
-                                    <div className='modal-container-details-text col-sm-4' dir='rtl'>
-                                        <p dir='rtl' className='modal-container-details-text-title'>טלפון: </p>
-                                        <p>{report.reporterDetails.phone}</p>
-                                    </div>
-                                    <div className='modal-container-details-text col-sm-4' dir='rtl'>
-                                        <p dir='rtl' className='modal-container-details-text-title'>שכונה: </p>
-                                        <p>{report.location.neighborhood}</p>
-                                    </div>
-
-                                    <div className='modal-container-details-text col-sm-4' dir='rtl'>
-                                        <p dir='rtl' className='modal-container-details-text-title'>רחוב: </p>
-                                        <p>{report.location.street}</p>
-                                    </div>
-
-
-                                </div>
-                                <div className='modal-container-details-short-inputs-lowe row' dir='rtl'>
-
-                                    <div className='modal-container-details-text col-sm-4' dir='rtl'>
-                                        <p dir='rtl' className='modal-container-details-text-title'>גודל: </p>
-                                        <p>{report.dogDetails.size}</p>
-                                    </div>
-
-                                    <div className='modal-container-details-text col-sm-4' dir='rtl'>
-                                        <p dir='rtl' className='modal-container-details-text-title'>צבע: </p>
-                                        <p>{report.dogDetails.color}</p>
-                                    </div>
-
-                                    <div className='modal-container-details-text col-sm-4' dir='rtl'>
-                                        <p dir='rtl' className='modal-container-details-text-title'>אלים: </p>
-                                        <p>{report.dogDetails.violent}</p>
-                                    </div>
+                            <div className='animal-modal-img'>
+                                <div className='animal-modal-img-container'>
+                                    <img className='animal-modal-img-container-image' src={report.reportDetails.picture.url} alt="" />
+                                    {/* <Image cloudName="diggwedxe" publicId={user.avatar.public_id} className="img-thumbnail"/> */}
                                 </div>
                             </div>
-                        </div>
-                        <br></br>
-                        <div className='modal-container-datetime'>
-                            <h5 className="card-title">{report.reportDetails.time.date}</h5>
-                            <h5 className="card-title">{report.reportDetails.time.hour}</h5>
-                        </div>
+                            <div className='modal-container-details'>
+                                <div className='modal-container-details-text' dir='rtl'>
+                                    <p dir='rtl' className='modal-container-details-text-title'>שם מדווח: </p>
+                                    <p>{report.reporterDetails.fullName}</p>
+                                </div>
+                                <div className='modal-container-details-text' dir='rtl'>
+                                    <p dir='rtl' className='modal-container-details-text-title'>תיאור: </p>
+                                    <p>{report.reportDetails.details}</p>
+                                </div>
+
+                                <div className='modal-container-details-short-inputs'>
+                                    <div className='modal-container-details-short-inputs-upper row' dir='rtl'>
+
+                                        <div className='modal-container-details-text col-sm-4' dir='rtl'>
+                                            <p dir='rtl' className='modal-container-details-text-title'>טלפון: </p>
+                                            <p>{report.reporterDetails.phone}</p>
+                                        </div>
+                                        <div className='modal-container-details-text col-sm-4' dir='rtl'>
+                                            <p dir='rtl' className='modal-container-details-text-title'>שכונה: </p>
+                                            <p>{report.location.neighborhood}</p>
+                                        </div>
+
+                                        <div className='modal-container-details-text col-sm-4' dir='rtl'>
+                                            <p dir='rtl' className='modal-container-details-text-title'>רחוב: </p>
+                                            <p>{report.location.street}</p>
+                                        </div>
 
 
-                    </Modal>
+                                    </div>
+                                    <div className='modal-container-details-short-inputs-lowe row' dir='rtl'>
 
-                    <div className="btn-group dropup">
-                        <button type="button" className="remove-borders" data-bs-toggle="dropdown" aria-expanded="false" data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
-                            <div className='more-options-icon-container'>
-                                <IoMdMore />
+                                        <div className='modal-container-details-text col-sm-4' dir='rtl'>
+                                            <p dir='rtl' className='modal-container-details-text-title'>גודל: </p>
+                                            <p>{report.dogDetails.size}</p>
+                                        </div>
+
+                                        <div className='modal-container-details-text col-sm-4' dir='rtl'>
+                                            <p dir='rtl' className='modal-container-details-text-title'>צבע: </p>
+                                            <p>{report.dogDetails.color}</p>
+                                        </div>
+
+                                        <div className='modal-container-details-text col-sm-4' dir='rtl'>
+                                            <p dir='rtl' className='modal-container-details-text-title'>אלים: </p>
+                                            <p>{report.dogDetails.violent}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='popper'>
-                                <p>עדכון סטאטוס</p>
+                            <br></br>
+                            <div className='modal-container-datetime'>
+                                <h5 className="card-title">{report.reportDetails.time.date}</h5>
+                                <h5 className="card-title">{report.reportDetails.time.hour}</h5>
                             </div>
-                        </button>
-                        <ul className="dropdown-menu" dir='rtl'>
-                            <li><a href='#notfound' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "לא נמצא", report, handleAlerts)}>לא נמצא</a></li>
-                            <li><a href='#teart' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "טופל בשטח ושוחרר", report, handleAlerts)}>טופל בשטח ושוחרר</a></li>
-                            {/* <li><a className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "טופל בשטח והועבר לוטרינריה", report)}>טופל בשטח והועבר לוטרינריה</a></li> */}
-                            <li><a href='#vet' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "הועבר לוטרינירה", report, handleAlerts)}>הועבר לוטרינריה</a></li>
-                            <li><a href= '#dead'className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "החיה נמצאה מתה בשטח", report, handleAlerts)}>החיה נמצאה מתה בשטח</a></li>
 
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><a href='#delete' className="dropdown-item detele-report " dir='rtl' onClick={() => deleteStatus(dispatch, "מחיקת דיווח", report, handleAlerts)}>מחיקת דיווח</a></li>
-                        </ul>
+
+                        </Modal>
+
+                        <div className="btn-group dropup">
+                            <button type="button" className="remove-borders" data-bs-toggle="dropdown" aria-expanded="false" data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
+                                <div className='more-options-icon-container'>
+                                    <IoMdMore />
+                                </div>
+                                <div className='popper'>
+                                    <p>עדכון סטאטוס</p>
+                                </div>
+                            </button>
+                            <ul className="dropdown-menu" dir='rtl'>
+                                <li><a href='#notfound' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "לא נמצא", report, handleAlerts)}>לא נמצא</a></li>
+                                <li><a href='#teart' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "טופל בשטח ושוחרר", report, handleAlerts)}>טופל בשטח ושוחרר</a></li>
+                                {/* <li><a className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "טופל בשטח והועבר לוטרינריה", report)}>טופל בשטח והועבר לוטרינריה</a></li> */}
+                                <li><a href='#vet' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "הועבר לוטרינירה", report, handleAlerts)}>הועבר לוטרינריה</a></li>
+                                <li><a href='#dead' className="dropdown-item" dir='rtl' onClick={() => updateStatus(dispatch, "החיה נמצאה מתה בשטח", report, handleAlerts)}>החיה נמצאה מתה בשטח</a></li>
+
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a href='#delete' className="dropdown-item detele-report " dir='rtl' onClick={() => deleteStatus(dispatch, "מחיקת דיווח", report, handleAlerts)}>מחיקת דיווח</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Alert alertType={alertType} alert={alert} setAlert={setAlert}>
-            {alertMessage}
-        </Alert>
+            <Alert alertType={alertType} alert={alert} setAlert={setAlert} unwidth={true}>
+                {alertMessage}
+            </Alert>
         </>
     );
 }
--- veterinarian/src/Components/alert/Alert.jsx
+++ veterinarian/src/Components/alert/Alert.jsx
@@ -1,18 +1,18 @@
 import { useEffect } from 'react'
 import './alert.css'
 
-const Alert  = ({ alert, alertType, setAlert, children }) => {
+const Alert = ({ alert, alertType, setAlert, unwidth, children }) => {
 
     useEffect(() => {
         setTimeout(() => {
             setAlert(false);
-          }, 2000);
+        }, 2000);
     }, [alert, setAlert])
 
-    return(
-        <div className="alert-container"> 
+    return (
+        <div className={unwidth ? "alert-container-unwidth" : "alert-container"}>
             <div className={`alert alert-${alertType} save-spot-alert right-to-left mb-4`} role="alert" hidden={!alert}>
-               {children}
+                {children}
             </div>
         </div>
     )
